d39882189c420c80de921a1923a27a5993fd59ba
rachetm <rmudnur@gmail.com>
2019-11-10 15:13:42 +0530

    Changes

--- config/api.js
+++ config/api.js
@@ -1,69 +1,40 @@
-const GetJokeHandler1 = {
-    canHandle(handlerInput) {
-     //const request = handlerInput.requestEnvelope.request;
-      //return request.type === 'IntentRequest'
-        //&& request.intent.name === 'GetJokeIntent';
-          const request = handlerInput.requestEnvelope.request;
-          return request.type === 'LaunchRequest'
-        ||  (request.type === 'IntentRequest'
-          && request.intent.name === 'GetJokeHandler')
-      
-       
-    },
-    
     var Request = require('request');
 
-    async handle(handlerInput) {
-      const response = await getDetails(); 
+      const res = getDetails(); 
       
         if(res.status === 200)
         {
+            const ingredients = res.product[0].ingredients;
+            
+            let flag = false;
+
+            const usersAllergy = ['nuts', 'milk']; //this will come from SQL DB in array form
+
+            ingredients.forEach(e_1 => {
+                usersAllergy.forEach(e_2 => {
+                    if(e_1 == e_2)
+                    {
+                        flag = true;
+                        // break;
+                    }
+                });
+            });
+
+            if(flag)
+
+                // .speak("You cannot have this product"); //tell user that he can or cannot have the product
 
         }
         else
         {
-            res.message
+            console.log(res.message); //or 'res.message' can be given to .speak() directly
         }
-
-    //   console.log(response);
-  
-    //   return handlerInput.responseBuilder
-    //           .speak("Okay. Here is what I got back from my request, for you anurag " + response.value.joke)
-    //           .reprompt("What would you like?")
-    //           .getResponse();
-    },
-  };
-
   
   
   function getDetails() {
     return new Promise(((resolve, reject) => {
-    //   var options = {
-    //       host: 'https://dietary-care.herokuapp.com/',
-    //       port: 22436,
-    //       path: '/jokes/random',
-    //       method: 'GET',
-    //   };
-      
-    //   const request = https.request(options, (response) => {
-    //     response.setEncoding('utf8');
-    //     let returnData = '';
-  
-    //     response.on('data', (chunk) => {
-    //       returnData += chunk;
-    //     });
-  
-    //     response.on('end', () => {
-    //       resolve(JSON.parse(returnData));
-    //     });
-  
-    //     response.on('error', (error) => {
-    //       reject(error);
-    //     });
-    //   });
-    //   request.end();
-
-    const product = nutella; //this should be captured from the user's query
+   
+    const product = "nutella"; //this should be captured from the user's query
 
     Request.get(`https://dietary-care.herokuapp.com/${product}`, function(err, res, body)
     {
