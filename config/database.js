d02e13b760b9612b42579aa9f8410287d7d7c81a
rachetm <rmudnur@gmail.com>
2019-11-10 11:08:29 +0530

    Changes

--- config/database.js
+++ config/database.js
@@ -1,5 +1,6 @@
 module.exports = {
-    database : "mongodb://dietary_care:sudohexa56@ds149146.mlab.com:49146/heroku_6hz92z86"
+    database: "mongodb+srv://admin:sudohexa56@dietary-care-xtrun.mongodb.net/dietarycare?retryWrites=true&w=majority"
+    // database : "mongodb://dietary_care:sudohexa56@ds149146.mlab.com:49146/heroku_6hz92z86"
     // database: "mongodb+srv://dietaryCare_lambda:sudohexa56@dietaryCare-pqqnb.mongodb.net/dietarycare?retryWrites=true&w=majority"
     // database: "mongodb://localhost:27017/dietarycare",
 };
