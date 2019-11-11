305b14265ae48a09b7d2908641c237cbc58ebb0f
rachetm <rmudnur@gmail.com>
2019-11-11 15:15:04 +0530

    Changes

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    // database: "mongodb+srv://dietarycare_rw:sudohexa56@dietary-care-xtrun.mongodb.net/dietarycare?retryWrites=true&w=majority"
-    database: "mongodb://localhost:27017/dietarycare",
+    database: "mongodb+srv://dietarycare_rw:sudohexa56@dietary-care-xtrun.mongodb.net/dietarycare?retryWrites=true&w=majority"
+    // database: "mongodb://localhost:27017/dietarycare",
 };
