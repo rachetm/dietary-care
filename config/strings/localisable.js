8410ab01458e3493ff0574f2ee48eecb05c646a2
Rachet Mudnur <rmudnur@gmail.com>
2020-06-11 01:21:45 +0530

    Minor change

--- config/strings/localisable.js
+++ config/strings/localisable.js
@@ -1,7 +1,7 @@
 const localisable = {
     welcomeMessage: 'Welcome to dietary-care food products API',
     endpoint_1_description: 'Displays all products',
-    endpoint_2_description: 'Lets you search for particular product by providing search query',
+    endpoint_2_description: 'Lets you check if you are allergic to a certain product',
     endpoint_3_description: 'Lets you add a product to the database',
     endpoint_4_description: 'Get product recommendation based on query parameters',
     success: 'Success',
